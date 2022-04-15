0bf1225fbc68eeeea6b86fd7d75eaa6eba350a36
Maula Izza Azizi <72302421+storyofhis@users.noreply.github.com>
2022-04-15 19:20:18 +0700

    feat: create and update data

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+class Post extends Model
+{
+    // use HasFactory;
+    use softDeletes;
+
+    protected $table = 'mahasiswa';
+    protected $fillable = [
+        'nama',
+        'nim',
+        'alamat',
+    ];
+    protected $hidden;
+}
